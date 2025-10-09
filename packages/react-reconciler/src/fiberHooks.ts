9f01ea41b3f7381c9a35a224cef56215168bd1a8
Percy Kuang <percykuang@foxmail.com>
2025-10-09 14:38:46 +0800

    feat: 实现Hooks架构

--- packages/react-reconciler/src/fiberHooks.ts
+++ packages/react-reconciler/src/fiberHooks.ts
@@ -1,8 +1,38 @@
 import { FiberNode } from './fiber';
 
+// import internals from 'shared/internals';
+
+let currentlyRenderingFiber: FiberNode | null = null;
+// let workInProgressHook: Hook | null = null;
+
+// const { currentDispatcher } = internals;
+
+interface Hook {
+	memoizedState: any;
+	updateQueue: unknown;
+	next: Hook | null;
+}
+
 export function renderWithHooks(wip: FiberNode) {
+	// 赋值
+	currentlyRenderingFiber = wip;
+	wip.memoizedState = null;
+
+	const current = wip.alternate;
+
+	if (current !== null) {
+		// update
+	} else {
+		// mount
+		// currentDispatcher
+	}
+
 	const Component = wip.type;
 	const props = wip.pendingProps;
 	const children = Component(props);
+
+	// 重置
+	currentlyRenderingFiber = null;
+
 	return children;
 }
--- scripts/rollup/react-dom.config.js
+++ scripts/rollup/react-dom.config.js
@@ -3,7 +3,7 @@ import generatePackageJson from 'rollup-plugin-generate-package-json';
 
 import { getBaseRollupPlugins, getPackageJSON, resolvePkgPath } from './utils';
 
-const { name, module } = getPackageJSON('react-dom');
+const { name, module, peerDependencies } = getPackageJSON('react-dom');
 // react dom 包的路径
 const pkgPath = resolvePkgPath(name);
 // react dom 产物路径
@@ -26,6 +26,7 @@ export default [
 				format: 'umd'
 			}
 		],
+		externals: [...Object.keys(peerDependencies)],
 		plugins: [
 			...getBaseRollupPlugins(),
 			alias({
